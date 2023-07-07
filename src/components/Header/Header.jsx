9a0b7015705685ad2edfad29aa3e0b2879e4a7a4
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2023-07-08 02:16:58 +0530

    Shopping Cart  static done succesfully

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -16,6 +16,7 @@ import "./Header.scss";
 
 const Header = () => {
   const [scrolled, setScrolled] = useState(false);
+  const [showCart, setShowCart] = useState(false);
 
   const handleScroll = () => {
     const offset = window.scrollY;
@@ -30,24 +31,32 @@ const Header = () => {
     window.addEventListener("scroll", handleScroll);
   }, []);
   return (
-    <header className={`main-header ${scrolled ? "sticky-header" : ""}`}>
-      <div className="header-content">
-        <ul className="left">
-          <li>Home</li>
-          <li>About</li>
-          <li>Categories</li>
-        </ul>
-        <div className="center">SDEVSTORE</div>
-        <div className="right">
-          <TbSearch />
-          <AiOutlineHeart />
-          <span className="cart-icon">
-            <CgShoppingCart />
-            <span>5</span>
-          </span>
+    <>
+      <header className={`main-header ${scrolled ? "sticky-header" : ""}`}>
+        <div className="header-content">
+          <ul className="left">
+            <li>Home</li>
+            <li>About</li>
+            <li>Categories</li>
+          </ul>
+          <div className="center">SDEVSTORE</div>
+          <div className="right">
+            <TbSearch />
+            <AiOutlineHeart />
+            <span
+              className="cart-icon"
+              onClick={() => {
+                setShowCart(true);
+              }}
+            >
+              <CgShoppingCart />
+              <span>5</span>
+            </span>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+      {showCart && <Cart setShowCart={setShowCart} />}
+    </>
   );
 };
 
