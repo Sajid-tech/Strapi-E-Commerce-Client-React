921db45c7e8bc1f496c97b63feef2f62bdcde9df
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2023-07-07 22:33:53 +0530

    Category Page Done

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import "./Products.scss";
 import Product from "./Product/Product";
 
-const Products = () => {
+const Products = ({ innerPage }) => {
   return (
     <div className="products-container">
-      <div className="sec-heading">Section Heading</div>
+      {/* its means if you are in inner category page than don't show Section heading only if innerPage is true -->see( catogry and products).jsx both for understanding */}
+      {!innerPage && <div className="sec-heading"> Section Heading </div>}
       <div className="products">
         <Product />
         <Product />
