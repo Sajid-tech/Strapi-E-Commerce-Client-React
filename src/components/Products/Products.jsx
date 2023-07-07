bb19fb5fc9c8d27767b790638f60d36687a7fc83
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2023-07-08 01:26:52 +0530

    Related Products for Products details done

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import "./Products.scss";
 import Product from "./Product/Product";
 
-const Products = ({ innerPage }) => {
+const Products = ({ innerPage, headingText }) => {
   return (
     <div className="products-container">
       {/* its means if you are in inner category page than don't show Section heading only if innerPage is true -->see( catogry and products).jsx both for understanding */}
-      {!innerPage && <div className="sec-heading"> Section Heading </div>}
+      {!innerPage && <div className="sec-heading"> {headingText} </div>}
       <div className="products">
         <Product />
         <Product />
