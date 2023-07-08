cd6ed48ed1a3fd5686b35ea32c968ff31f1587cf
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2023-07-09 01:08:12 +0530

    Product Details Page Related Products Api Integration done

--- src/components/SingleProduct/SingleProduct.jsx
+++ src/components/SingleProduct/SingleProduct.jsx
@@ -81,7 +81,10 @@ const SingleProduct = () => {
             </div>
           </div>
         </div>
-        <RelatedProducts />
+        <RelatedProducts
+          product={id}
+          categoryId={product.categories.data[0].id}
+        />
       </div>
     </div>
   );
