63bdb47c36f09841b4b950a6261fec2ef8a7db1d
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2023-07-08 22:54:14 +0530

    Homepage Products Api Integration done

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,20 +2,15 @@ import React from "react";
 import "./Products.scss";
 import Product from "./Product/Product";
 
-const Products = ({ innerPage, headingText }) => {
+const Products = ({ products, innerPage, headingText }) => {
   return (
     <div className="products-container">
       {/* its means if you are in inner category page than don't show Section heading only if innerPage is true -->see( catogry and products).jsx both for understanding */}
       {!innerPage && <div className="sec-heading"> {headingText} </div>}
       <div className="products">
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
+        {products?.data?.map((item) => (
+          <Product key={item.id} id={item.id} data={item.attributes} />
+        ))}
       </div>
     </div>
   );
